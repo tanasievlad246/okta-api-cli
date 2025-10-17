e7689e2b004dee1d7883c1c0a691f561228497e0
Tanasie Vlad <tanasievladcristian@gmail.com>
2025-10-17 19:33:01 +0300

    Added new version in the __version__ file

--- cli/__init__.py
+++ cli/__init__.py
@@ -4,4 +4,4 @@ Okta CLI - Command line interface for Okta API.
 This package provides CLI commands for managing Okta users with local SQLite caching.
 """
 
-__version__ = "0.1.0"
+__version__ = "0.2.0"
